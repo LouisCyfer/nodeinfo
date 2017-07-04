a0b9083f44f3689cdf5589c8ed8092a277b687b7
LouisCyfer <LouisCyfer@users.noreply.github.com>
2017-07-04 18:53:34 +0200

    v0.3
    - HUD readjustments (clock is now bottom right)
    - added basic_machines support (descriptions)
    - minor bugfixes

--- init.lua
+++ init.lua
@@ -2,6 +2,21 @@ player_identifier_table = {}
 local border_size = 0.2
 local clearImg = "bg.png"
 
+--[[
+-- possibly necessary for future updates
+
+local basic_machines_found = false
+
+function applyModSupport()
+	for i, name in ipairs(minetest.get_modnames()) do
+		if name == "basic_machines" then
+			basic_machines_found = true
+		end
+	end
+end
+applyModSupport()
+--]]
+
 minetest.register_on_joinplayer(function(player)
 
 	local name = player:get_player_name()
@@ -11,42 +26,31 @@ minetest.register_on_joinplayer(function(player)
 	end
 
 	player_identifier_table[name].name = "air"
-
-	player_identifier_table[name].clockbackground = player:hud_add({
-		hud_elem_type = "image",
-		position = {x=0.96,y=0.004},
-		size = "",
-		text = "bg.png",
-		number = 20,
-		alignment = {x=1,y=1},
-		offset = {x=0, y=0},
-		scale = {x=4.5,y=1.2 + border_size}
-	})
-
-	player_identifier_table[name].clocktext = player:hud_add({
+--[[
+	player_identifier_table[name].huddebug = player:hud_add({
 		hud_elem_type = "text",
-		position = {x=0.961,y=0.0075},
+		position = {x=0,y=0},
 		size = "",
-		text = "0:00:00",
-		number = 0xFFFFFF,
+		text = "DEBUG:",
+		number = 0x00FF00,
 		alignment = {x=1,y=1},
 		offset = {x=0, y=0}
 	})
-
+--]]
 	player_identifier_table[name].hudbackground = player:hud_add({
 		hud_elem_type = "image",
-		position = {x=0.7,y=0.878},
+		position = {x=0.65,y=0.92},
 		size = "",
 		text = "bg.png",
 		number = 20,
 		alignment = {x=1,y=1},
 		offset = {x=0, y=0},
-		scale = {x=25,y=4.8 + border_size}
+		scale = {x=40.5,y=4.8 + border_size}
 	})
 
 	player_identifier_table[name].hudtitle = player:hud_add({
 		hud_elem_type = "text",
-		position = {x=0.705,y=0.885},
+		position = {x=0.655,y=0.927},
 		size = "",
 		text = "Block-Info:",
 		number = 0x00FF00,
@@ -56,7 +60,7 @@ minetest.register_on_joinplayer(function(player)
 
 	player_identifier_table[name].hudimage = player:hud_add({
 		hud_elem_type = "image",
-		position = {x=0.705,y=0.905},
+		position = {x=0.66,y=0.95},
 		size = "",
 		text = "bg.png",
 		number = 20,
@@ -66,9 +70,19 @@ minetest.register_on_joinplayer(function(player)
 
 	player_identifier_table[name].hudtext = player:hud_add({
 		hud_elem_type = "text",
-		position = {x=0.75,y=0.885},
+		position = {x=0.72,y=0.927},
 		size = "",
 		text = "",
+		number = 0xFFFFFF,
+		alignment = {x=1,y=1},
+		offset = {x=0, y=0}
+	})
+
+	player_identifier_table[name].clocktext = player:hud_add({
+		hud_elem_type = "text",
+		position = {x=0.962,y=0.977},
+		size = "",
+		text = "0:00:00",
 		number = 0xFFFFFF,
 		alignment = {x=1,y=1},
 		offset = {x=0, y=0}
@@ -84,14 +98,15 @@ minetest.register_globalstep(function(dtime)
 		local distance
 		local newdir
 		local newlook
-		local name
+		local pName = player:get_player_name()
 		local oldnode
 
-		local setText
+		local text_title
+		local text_description
+		local setImg
 		local tiles
 
-		oldnode = player_identifier_table[player:get_player_name()].name
-		name = player:get_player_name()
+		oldnode = player_identifier_table[pName].name
 		newdir   = {}
 		newlook  = {}
 		distance = 3
@@ -112,31 +127,60 @@ minetest.register_globalstep(function(dtime)
 			local node = minetest.get_node({x=newdir.x,y=newdir.y,z=newdir.z})
 			if node.name ~= "air" and node.name ~= "ignore" then
 				if node.name ~= oldnode then
+					player_identifier_table[pName].name = node.name
+					setImg = clearImg
 
-					local setImg = clearImg
-
-					if minetest.registered_nodes[node.name].tiles ~= nil and type(minetest.registered_nodes[node.name].tiles[1]) == "string" then
+					if minetest.registered_nodes[node.name].tiles[1] and type(minetest.registered_nodes[node.name].tiles[1]) == "string" then
 						setImg = minetest.registered_nodes[node.name].tiles[1]
+
+						if minetest.registered_items[node.name].inventory_image then
+							setImg = minetest.registered_items[node.name].tiles[1]
+						end
 					end
 
-					player:hud_change(player_identifier_table[name].hudimage, "text", setImg)
+					player:hud_change(player_identifier_table[pName].hudimage, "text", setImg)
 
-					setText = minetest.registered_nodes[node.name].description .. "\n\n" .. node.name
+					local getStr = minetest.registered_nodes[node.name].description
+					text_title = getStr
 
-					player_identifier_table[player:get_player_name()].name = setText
+					local checkStr = " - "
 
-					player:hud_change(player_identifier_table[name].hudtext, "text", setText)
+					if string.find(getStr, checkStr) then
+						text_title = string.split(getStr, checkStr)[1]
+						text_description = string.split(getStr, checkStr)[2]
+					end
+
+					checkStr = ": "
+
+					if string.find(getStr, checkStr) then
+						text_title = string.split(getStr, checkStr)[1]
+						text_description = string.split(getStr, checkStr)[2]
+					end
+
+					if text_description == nil then
+						text_description = " "
+					end
+
+					local splitIndex = 0
+
+					if string.len(text_description) > 60 then
+						splitIndex = string.find(text_description, " ", 60)
+						text_description = string.sub(text_description, 0, splitIndex) .. "\n" .. string.sub(text_description, splitIndex + 1)
+					end
+
+					text_title = text_title .. " (" .. node.name .. ") | Mod: " .. minetest.registered_nodes[node.name].mod_origin .. "\n\n"
+					player:hud_change(player_identifier_table[pName].hudtext, "text", text_title .. text_description)
 				end
 				return
 			end
 		end
-		if player_identifier_table[player:get_player_name()].name ~= "Air" then
-			player:hud_change(player_identifier_table[name].hudimage, "text", clearImg)
+		if player_identifier_table[pName].name ~= "air" then
+			player:hud_change(player_identifier_table[pName].hudimage, "text", clearImg)
 
 			local node = "air"
 
-			player_identifier_table[player:get_player_name()].name = node
-			player:hud_change(player_identifier_table[name].hudtext, "text", node)
+			player_identifier_table[pName].name = node
+			player:hud_change(player_identifier_table[pName].hudtext, "text", node)
 		end
 	end
 end)
